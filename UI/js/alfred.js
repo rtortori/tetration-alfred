49652cbac3bc84d3376cc20ca85ad15fd82bf47e
rtortori <rtortori@cisco.com>
2018-02-10 01:25:57 +0100

    added tetration and apic connect button - minor fixes

--- UI/js/alfred.js
+++ UI/js/alfred.js
@@ -4,10 +4,12 @@ alfred.js
 
 */
 
+"use strict";
+
 // Start Welcome modal at page load
 $(window).on('load',function(){
-        $('#welcomeModal').modal('show');
-    });
+	$('#welcomeModal').modal('show');
+});
 
 
 // Set content type to JSON for POST calls
@@ -25,6 +27,7 @@ var serviceAPI = "http://" + alfredEndpoint + ":5000/api/v1/service"
 var brokerAPI = "http://" + alfredEndpoint + ":5000/api/v1/broker"
 var tetrationAPI = "http://" + alfredEndpoint + ":5000/api/v1/tetration"
 var apicAPI = "http://" + alfredEndpoint + ":5000/api/v1/apic"
+var endpointAPI = "http://" + alfredEndpoint + ":5000/api/v1/endpoints"
 
 // Buttons variables
 var refreshStatusButton = $("#refresh-status-button");
@@ -33,15 +36,24 @@ var submitConfirm = $("#submitConfirm");
 // Modal Variables
 var submitModal = $("#submitModal");
 
-// Alfred Service
+// Services and endpoints
 
-var alfredStatus = "Unknown"
+var alfredStatus = "Unknown";
+var tetrationStatus = "Unknown";
+var apicStatus = "Unknown";
+var tetrationURL = "Unknown";
+var apicURL = "Unknown";
+
 var alfredStatusLabel = $("#alfred-status-label");
+var tetrationStatusLabel = $("#tetration-status-label");
+var apicStatusLabel = $("#apic-status-label");
 
 var alfredStartButton = $("#alfredStart");
 var alfredStopButton = $("#alfredStop");
 var alfredRestartButton = $("#alfredRestart");
 
+
+// Get Alfred Status function
 
 function getAlfredStatus(){
 	$.getJSON(serviceAPI, function(result){
@@ -64,19 +76,46 @@ function getAlfredStatus(){
 	})
 };
 
+// Get endpoints status
+
+function getEndpointsStatus(){
+	$.getJSON(endpointAPI, function(result){
+		tetrationStatus = result.tetration_status
+		apicStatus = result.tetration_status
+		if (tetrationStatus == "reachable") {
+			tetrationStatusLabel.html("<span uk-icon='icon: check; ratio: 0.5'>Reachable</span>")
+			tetrationStatusLabel.removeClass("label-danger")
+			tetrationStatusLabel.addClass("label-success")
+		} else if (tetrationStatus == "down") {
+			tetrationStatusLabel.html("<span uk-icon='icon: ban; ratio: 0.5'>Unreachable</span>")
+			tetrationStatusLabel.removeClass("label-success")
+			tetrationStatusLabel.addClass("label-danger")
+		}
+		if (apicStatus == "reachable") {
+			apicStatusLabel.html("<span uk-icon='icon: check; ratio: 0.5'>Reachable</span>")
+			apicStatusLabel.removeClass("label-danger")
+			apicStatusLabel.addClass("label-success")			
+		} else if (apicStatus == "down") {
+			apicStatusLabel.html("<span uk-icon='icon: ban; ratio: 0.5'>Unreachable</span>")
+			apicStatusLabel.removeClass("label-success")
+			apicStatusLabel.addClass("label-danger")
+		}	
+	})
+};
 
 // Fetch services status every minute (30s)
 
 setInterval(function() {
 	getAlfredStatus();	
+	getEndpointsStatus();
 }, 30000);
-
 
 // Refresh button action
 
 refreshStatusButton.on("click", function(){
 	
 	getAlfredStatus();
+	getEndpointsStatus();
 
 });
 
@@ -148,19 +187,19 @@ function submitConfigForm() {
 	// console.log("submitted payload " + tetrationAPIPayload)
 
 		// Broker config
-	var brokerAPIPayload = '{"broker_ip": "' + $("#kafka-ip").val() + '","broker_port":"' +  $("#kafka-port").val()   + '","topic":"' + $("#kafka-topic").val()  + '"}'
-	
-	$.post(brokerAPI, brokerAPIPayload)
-	.done(function(data){		
-	});
+		var brokerAPIPayload = '{"broker_ip": "' + $("#kafka-ip").val() + '","broker_port":"' +  $("#kafka-port").val()   + '","topic":"' + $("#kafka-topic").val()  + '"}'
+
+		$.post(brokerAPI, brokerAPIPayload)
+		.done(function(data){		
+		});
 	// console.log("submitted payload " + brokerAPIPayload)
 
 		// APIC config
-	var apicAPIPayload = '{"apic_ip": "' + $("#apic-endpoint").val() + '","apic_port":"' +  $("#apic-port").val()   + '","apic_user":"' + $("#apic-user").val()  + '","apic_password":"' + $("#apic-password").val() + '"}'
-	
-	$.post(apicAPI, apicAPIPayload)
-	.done(function(data){		
-	});
+		var apicAPIPayload = '{"apic_ip": "' + $("#apic-endpoint").val() + '","apic_port":"' +  $("#apic-port").val()   + '","apic_user":"' + $("#apic-user").val()  + '","apic_password":"' + $("#apic-password").val() + '"}'
+
+		$.post(apicAPI, apicAPIPayload)
+		.done(function(data){		
+		});
 	// console.log("submitted payload " + apicAPIPayload)
 
 }
@@ -175,4 +214,19 @@ submitConfirm.on("click", function(){
 
 });
 
+// Set APIC and Tetration connect buttons
+
+var tetrationConnectButton = $("#tetration-connect-btn");
+var apicConnectButton = $("#apic-connect-btn");
+
+$.getJSON(tetrationAPI, function(result){
+	tetrationURL = result.API_ENDPOINT
+	tetrationConnectButton.html("<a target='_blank' class='btn btn-sm btn-default' href='" + tetrationURL + "' id='tetration-connect-btn'>Connect</a>");
+});
+
+$.getJSON(apicAPI, function(result){
+	apicURL = result.apic_ip
+	apicConnectButton.html("<a target='_blank' class='btn btn-sm btn-default' href='" + apicURL + "' id='apic-connect-btn'>Connect</a>");
+
+})
 
