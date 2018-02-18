0ff1339566e5d0f82e1deceeb04ec4f9aa0bd2a7
rtortori <rtortori@cisco.com>
2018-02-18 17:37:08 +0100

    feature check needs a fix as JS True != Py true

--- tetration_alfred.py
+++ tetration_alfred.py
@@ -96,88 +96,86 @@ while True:
             if query_type == 'get_endpoint_detail':
                 log('alfred','debug','New incoming ACI annotation request for: {}'.format(payload))
                 # Tetration MUST pass the list of endpoints into a list
-                if configuration['aci_annotations_enabled'] == "true":
-                    for item in payload:
+
+                for item in payload:
+                    # Debug
+                    if debug_mode:
+                        print(item)
+
+                    # Poll the messages from Kafka to commit the offset
+                    consumer.poll()
+                    #
+                    try:
+                        ep_detail = alfred_utils.fetch_ep_detail(item, apic_ip, apic_port, apic_user, apic_password)
+                        time_now = time.strftime("%d-%b-%Y-%H:%M:%S", time.gmtime())
+
+                        log('aci-annotations', 'debug', 'Query for: {}'.format(item))
+
                         # Debug
                         if debug_mode:
-                            print(item)
+                            print("Tenant: " + ep_detail[0] + "\r")
+                            print("Application: " + ep_detail[1] + "\r")
+                            print("EPG: " + ep_detail[2] + "\r")
+                            print("Encapsulation: " + ep_detail[3] + "\r")
+                            print("Leaf ID: " + ep_detail[4] + "\r")
+                            print("Learning Source: " + ep_detail[5] + "\r")
+                            print("ACI Info Date: " + time_now)
 
-                        # Poll the messages from Kafka to commit the offset
-                        consumer.poll()
-                        #
-                        try:
-                            ep_detail = alfred_utils.fetch_ep_detail(item, apic_ip, apic_port, apic_user, apic_password)
-                            time_now = time.strftime("%d-%b-%Y-%H:%M:%S", time.gmtime())
+                        # The fetch_ep_detail function will return a list
+                        _Tenant =  ep_detail[0]
+                        _Application =  ep_detail[1]
+                        _EPG =  ep_detail[2]
+                        _Encapsulation =  ep_detail[3]
+                        _Leaf_ID =  ep_detail[4]
+                        _Learning_Source =  ep_detail[5]
+                        _ACI_Info_Date =  time_now
 
-                            log('aci-annotations', 'debug', 'Query for: {}'.format(item))
+                        # Write CSV file
+                        alfred_utils.create_annotation(annotation_csv_file,
+                                                       item,
+                                                       VRF,
+                                                       _Tenant,
+                                                       _Application,
+                                                       _EPG,
+                                                       _Encapsulation,
+                                                       _Leaf_ID,
+                                                       _Learning_Source,
+                                                       _ACI_Info_Date)
 
-                            # Debug
-                            if debug_mode:
-                                print("Tenant: " + ep_detail[0] + "\r")
-                                print("Application: " + ep_detail[1] + "\r")
-                                print("EPG: " + ep_detail[2] + "\r")
-                                print("Encapsulation: " + ep_detail[3] + "\r")
-                                print("Leaf ID: " + ep_detail[4] + "\r")
-                                print("Learning Source: " + ep_detail[5] + "\r")
-                                print("ACI Info Date: " + time_now)
-
-                            # The fetch_ep_detail function will return a list
-                            _Tenant =  ep_detail[0]
-                            _Application =  ep_detail[1]
-                            _EPG =  ep_detail[2]
-                            _Encapsulation =  ep_detail[3]
-                            _Leaf_ID =  ep_detail[4]
-                            _Learning_Source =  ep_detail[5]
-                            _ACI_Info_Date =  time_now
-
-                            # Write CSV file
-                            alfred_utils.create_annotation(annotation_csv_file,
-                                                           item,
-                                                           VRF,
-                                                           _Tenant,
-                                                           _Application,
-                                                           _EPG,
-                                                           _Encapsulation,
-                                                           _Leaf_ID,
-                                                           _Learning_Source,
-                                                           _ACI_Info_Date)
-
-                            # Push Annotation to Tetration
-                            alfred_utils.tet_annotate('add', API_ENDPOINT, CREDENTIALS_FILE, annotation_csv_file, app_scope)
-                            log('aci-annotations', 'debug', 'Asset annotated in Tetration: {}'.format(item))
-
-                        except Exception:
-                            # Will trigger if the condition of the fetch_ep_detail function is not met
-                            if debug_mode:
-                                print('Endpoint not present')
-                            log('aci-annotations', 'error', 'Endpoint not present: {}'.format(item))
-                            pass
-
-                    if debug_mode:
-                        print('Done!')
-                else:
-                    log('alfred', 'error', 'ACI Annotations is not enabled')
-
-            elif query_type == 'dump_to_email':
-                if configuration['mail_server_enabled'] == "true":
-                    log('alfred', 'debug', 'New incoming Alarm. Forwarding message to mail server')
-
-                    consumer.poll()
-
-                    try:
-                        email('Tetration Alfred Alarm System',payload)
+                        # Push Annotation to Tetration
+                        alfred_utils.tet_annotate('add', API_ENDPOINT, CREDENTIALS_FILE, annotation_csv_file, app_scope)
+                        log('aci-annotations', 'debug', 'Asset annotated in Tetration: {}'.format(item))
 
                     except Exception:
+                        # Will trigger if the condition of the fetch_ep_detail function is not met
                         if debug_mode:
-                            print('An Error occurred when sending email')
-                        log('alfred', 'error', 'An Error occurred when sending email')
+                            print('Endpoint not present')
+                        log('aci-annotations', 'error', 'Endpoint not present: {}'.format(item))
                         pass
 
-                    if debug_mode:
-                        print('Done!')
+                if debug_mode:
+                    print('Done!')
 
-                else:
-                    log('alfred', 'error', 'New incoming alarm but Mail Server is not enabled')
+
+            elif query_type == 'dump_to_email':
+
+                log('alfred', 'debug', 'New incoming Alarm. Forwarding message to mail server')
+
+                consumer.poll()
+
+                try:
+                    email('Tetration Alfred Alarm System',payload)
+
+                except Exception:
+                    if debug_mode:
+                        print('An Error occurred when sending email')
+                    log('alfred', 'error', 'An Error occurred when sending email')
+                    pass
+
+                if debug_mode:
+                    print('Done!')
+
+
 
         # If the message is not properly formatted we will just ignore it
         except Exception:
